e4e30c851341cb997d2ac06d0583f9662de640e8
Phil Fearon <phil.fearon@deltaxml.com>
2021-11-07 12:41:43 +0000

    project compiles with semanticTokenProvider ts code added

--- browser-esm-webpack-typescript/src/index.ts
+++ browser-esm-webpack-typescript/src/index.ts
@@ -20,7 +20,90 @@ self.MonacoEnvironment = {
 	}
 };
 
-monaco.editor.create(document.body, {
+const themeRules: monaco.editor.ITokenThemeRule[] = [
+	{
+		token: 'xmlPunctuation',
+		foreground: '#ff0000'
+	}
+]
+
+const themeColors: monaco.editor.IColors = {'myred': '#ff0000'};
+
+const themeData: monaco.editor.IStandaloneThemeData = {
+    base: 'vs',
+    inherit: true,
+	rules: themeRules,
+	colors: themeColors
+}
+
+const legend = {
+    tokenTypes: [
+        'comment', 'string', 'keyword', 'number', 'regexp', 'operator', 'namespace',
+        'type', 'struct', 'class', 'interface', 'enum', 'typeParameter', 'function',
+        'member', 'macro', 'variable', 'parameter', 'property', 'label'
+    ],
+    tokenModifiers: [
+        'declaration', 'documentation', 'readonly', 'static', 'abstract', 'deprecated',
+        'modification', 'async'
+    ]
+};
+
+function getType(type: string) {
+    return legend.tokenTypes.indexOf(type);
+}
+
+const tokenPattern = new RegExp('([a-zA-Z]+)((?:\\.[a-zA-Z]+)*)', 'g');
+
+monaco.editor.defineTheme('myTheme', themeData);
+
+monaco.languages.registerDocumentSemanticTokensProvider('plaintext', {
+	getLegend: function () {
+			return legend;
+	},
+	provideDocumentSemanticTokens: function (model, lastResultId, token) {
+			const lines = model.getLinesContent();
+
+			/** @type {number[]} */
+			const data = [];
+
+			let prevLine = 0;
+			let prevChar = 0;
+
+			for (let i = 0; i < lines.length; i++) {
+					const line = lines[i];
+
+					for (let match = null; match = tokenPattern.exec(line);) {
+							// translate token and modifiers to number representations
+							let type = getType(match[1]);
+							if (type === -1) {
+									continue;
+							}
+							let modifier = 0
+
+							data.push(
+									// translate line to deltaLine
+									i - prevLine,
+									// for the same line, translate start to deltaStart
+									prevLine === i ? match.index - prevChar : match.index,
+									match[0].length,
+									type,
+									modifier
+							);
+
+							prevLine = i;
+							prevChar = match.index;
+					}
+			}
+			return {
+					data: new Uint32Array(data),
+					resultId: null
+			};
+	},
+	releaseDocumentSemanticTokens: function (resultId) { }
+});
+
+const mEditor = monaco.editor.create(document.body, {
 	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-	language: 'typescript'
+	language: 'typescript',
+	theme: 'dark'
 });
